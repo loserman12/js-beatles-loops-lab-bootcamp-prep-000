8290120e892044072608adc8c19927775d528d90
ralphcuello1016@gmail.com <ralphcuello1016@gmail.com>
2018-05-26 21:36:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-function theBeatlesPlay (musicians, instruments) {
+function theBeatlesPlay(musicians, instruments){
   var array = [];
-  for ( var i = 0; i < musicians.length; i++) {
-    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  for (var i = 0; i < musicians.length; i++){
+   array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return array;
 }
 
-function johnLennonFacts(array) {
-  const facts = [];
+
+function johnLennonFacts(facts){
+  var array = [];
   let i = 0;
- while (i < array.length) {
-   facts.push(`${array[i]}!!!`)
-   i++;
-   }
- return facts;
+  while (i < facts.length) {
+    array.push(`${facts[i]}!!!`)
+    i++;
+  }
+return array;
 }
 
-function iLoveTheBeatles (n) {
-  var ten = [];
-  do {
-ten.push("I love the Beatles!");
-n++
-  } while (n < 15 )
-  
- return ten;
+function iLoveTheBeatles (number){
+  var array = [];
+  do { array.push(`I love the Beatles!`); number++
   }
+  while (number < 15)
+  return array;
+}
+
+
